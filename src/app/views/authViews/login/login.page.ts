ae03cea453afd716cb92b0e00855f7a35cbd7745
sai kumar SKK <sai.korthivada@xcubelabs.com>
2019-02-18 00:43:20 +0530

    Implemented navigation

--- src/app/views/authViews/login/login.page.ts
+++ src/app/views/authViews/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private _router: Router) { }
 
   ngOnInit() {
+  }
+
+  register() {
+    this._router.navigate(['register']);
+  }
+
+  forgetPassword() {
+    this._router.navigate(['forgot-password']);
   }
 
 }
